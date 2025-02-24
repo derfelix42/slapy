74a3c2682441488cc43a73423957cfc212c12ff3
Felix Viola <felix.viola@uni-bonn.de>
2025-02-24 23:10:57 +0100

    adding documentation and contact

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+//! Slapy
+//!
+//! A much faster alternative to Pythons Scapy Library.
+//! Process PCAP files of captured Wi-Fi traffic.
 fn main() {
     println!("This is slapy - a much faster scapy alternative!");
 }
